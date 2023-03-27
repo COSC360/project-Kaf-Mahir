4639b0ebe0b823d88efbec7ae4a3564342d63e26
AK-121 <abdullah.kaff11@gmail.com>
2023-03-26 22:43:55 -0700

    edited constants

--- config/constants.php
+++ config/constants.php
@@ -1,8 +1,8 @@
 <?php
 
-define('dbhost', 'localhost');
-define('dbuser', 'root');
+define('dbhost', 'cosc360.ok.ubc.ca');
+define('dbuser', '47359534');
 define('dbpass', '47359534');
-define('dbname', 'nature');
+define('dbname', 'db_47359534');
 
 ?>
