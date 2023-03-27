db19816b7d32be14cbb4c879e4b39aae6d82cee8
AK-121 <abdullah.kaff11@gmail.com>
2023-03-26 23:01:32 -0700

    db

--- config/constants.php
+++ config/constants.php
@@ -1,6 +1,6 @@
 <?php
 
-define('dbhost', 'localhost');
+define('dbhost', 'cosc360.ok.ubc.ca');
 define('dbuser', '47359534@localhost');
 define('dbpass', 'a123b123');
 define('dbname', 'db_47359534');
