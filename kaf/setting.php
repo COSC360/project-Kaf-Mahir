98fe0300504213cdb1a5d31b280de78d39358088
Mahir Rahman <67985978+mahirr476@users.noreply.github.com>
2023-04-07 16:16:06 -0400

    Fixed drop down in setting.php

--- kaf/setting.php
+++ kaf/setting.php
@@ -1,57 +1,69 @@
 <?php
-require "../controllers/auth.php";
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+require '../controllers/auth.php';
 require 'load-profile.php';
-  
- //update/create profile
- 
- if (isset($_POST['save-profile']) && validateFile()) {
-  if (isset($_SESSION['username'])) {
-    $username = $_SESSION['username'];
-    $full_name = $_POST['inputName'];
-    $bio = $_POST['inputBio'];
 
-    if (!empty($full_name && !empty($bio))) {
-    //Check if username already has profile information
-    $check_query = "SELECT * FROM `profiles` WHERE `username` = '$username'";
-    $result = mysqli_query($conn, $check_query);
-    if (mysqli_num_rows($result) > 0) {
-      $update_query = "UPDATE `profiles` SET `full_name` = '$full_name', `bio` = '$bio' WHERE `username` = '$username'";
-      if (mysqli_query($conn, $update_query)) {
-        $msg = "Profile successfully updated";
-        header("Location: profile.php?msg=" . urlencode($msg));
-        exit;
+if (isset($_POST['post_id'])) { //if ajax request triggered
+  if (isset($_SESSION['username'])){ //if user logged in
+    try {
+      $username = $_SESSION['username'];
+      $post_id = $_POST['post_id'];
+      $result = mysqli_query($conn, "SELECT * FROM posts WHERE PostID = $post_id");
+      $row = mysqli_fetch_assoc($result);
+      $n = $row['upvotes'];
+       // Check if the user has already liked the post
+      $upvoted_query = "SELECT * FROM upvotes WHERE PostID = '$post_id' AND Username = '$username'";
+      $upvoted_result = mysqli_query($conn, $upvoted_query);
+      if (mysqli_num_rows($upvoted_result) > 0) {
+        // The user has already liked the post, so remove the like
+        $delete_query = "DELETE FROM upvotes WHERE PostID = '$post_id' AND Username = '$username'";
+        mysqli_query($conn, $delete_query);
+        mysqli_query($conn, "UPDATE posts SET upvotes=$n-1 WHERE PostID = $post_id");
+      } else {
+        // The user has not liked the post, so add the like
+        $add_query = "INSERT INTO upvotes (PostID, Username) VALUES ('$post_id', '$username')";
+        if (mysqli_query($conn, $add_query)) {
+        //run second query only if first one succeeded
+        mysqli_query($conn, "UPDATE posts SET upvotes=$n+1 WHERE PostID = $post_id");
+      }
     }
-    } else {
-    $insert_query = "INSERT INTO profiles (username, full_name, bio) values (?, ?, ?)";
-    $statement = $conn -> prepare($insert_query);
-    $statement -> bind_param('sss', $username, $full_name, $bio);
-    // If user is successfully created, log them in and redirect to homepage
-    if ($statement -> execute()) {
-      $msg = "Profile successfully created";
-      header("Location: profile.php?msg=" . urlencode($msg));
-      exit;
-  } else {
-    die("Error: ". $statement -> error);
-  }   
+
+    $stmt = $conn->prepare("SELECT COUNT(*) FROM upvotes WHERE PostID = ?");
+    $stmt->bind_param("i", $post_id);
+    $stmt->execute();
+    
+    $result = $stmt->get_result();
+    $row = mysqli_fetch_array($result);
+    
+    $upvotes = $row[0];
+    
+    // Return the updated upvote count as a JSON object
+    $response = array('upvotes' => $upvotes);
+    echo json_encode($response);
+  
+    } catch (mysqli_sql_exception $e) {  // If a user tries to upvote a post twice.
+      echo "WOOP WOOP";
+  }
+} else {
+  echo "login";
 }
+exit();
 }
-}
- }
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="css/setting.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
-    <title>Profile</title>
+    <title>Setting</title>
 </head>
 <body >
   <nav class="navbar fixed-top navbar-expand-md navbar-light" style="background-color: rgba(255,255,255, 0.85)">
@@ -79,51 +91,44 @@ require 'load-profile.php';
                 class="navbar-toggler"
                 aria-controls="navbarNav"
                 aria-expanded="false"
-                aria-label="Toggle navigation">
-                
-            <i class="bi bi-caret-down arrowDown"></i>
-            </button>
+                aria-label="Toggle navigation"><i class="bi bi-caret-down arrowDown"></i></button>
             <div 
                 class="collapse navbar-collapse "  
                 id="navbarNav">
-                <form action="results.php" class="d-flex ms-auto" method="POST">
+                <form action="results.php" class=" form-control-lg d-flex ms-auto" method="POST">
                     <input type="search" name="search" placeholder="Search..." class="form-control me 2">
                     <button class="btn btn-outline-info bi bi-search" type="submit" name="submit-search"></button></button>
                 </form>
-
                 <ul class="navbar-nav ms-auto align-items-center">
-                <li class="nav-item  ">
-                        <a href="home.php" class="nav-link  "><h4><i class="bi bi-house"></i></h4></a>
-                    </li>    
-                <li class="nav-item  ">
-                        <a href="login.php?logout" class="nav-link"><h4><i class='bi bi-box-arrow-right'></i></h4></a>
-
-                    </li>
-                    <li class="nav-item ">
-                      <?php 
-                          if (isset($_SESSION['username'])) {
-                            echo "<a href='./setting.php' class='nav-link'><h4><i class='bi bi-gear'></i></h4></a>";
-                          } else {
-                            //what to show when user is not logged in instead of settings. if anything
-                          }                    
-                        ?>
-                    </li>
-
-                     <li class="nav-item ">
-                      <?php 
-                          if (isset($_SESSION['username'])) {
-                            echo "<a href='./home.php' class='nav-link logout-link'><h4><i class='bi bi-backspace'></i></h4></a>";
-                          } else {
-                            //what to show when user is not logged in instead of settings. if anything
-                          }                    
-                        ?>
-                    </li>
+                  <li class="nav-item  ">
+                      <a href="home.php" class="nav-link  "><h4><i class="bi bi-house"></i></h4></a>
+                  </li>    
+                  <li class="nav-item  ">
+                      <a href="login.php?logout" class="nav-link"><h4><i class='bi bi-box-arrow-right'></i></h4></a>
+                  </li>
+                  <li class="nav-item ">
+                    <?php 
+                        if (isset($_SESSION['username'])) {
+                          echo "<a href='./setting.php' class='nav-link'><h4><i class='bi bi-gear'></i></h4></a>";
+                        } else {
+                          //what to show when user is not logged in instead of settings. if anything
+                        }                    
+                      ?>
+                  </li>
+                  <li class="nav-item ">
+                    <?php 
+                        if (isset($_SESSION['username'])) {
+                          echo "<a href='./home.php?logout' class='nav-link logout-link'><h4><i class='bi bi-x-square'></i></h4></a>";
+                        } else {
+                          //what to show when user is not logged in instead of settings. if anything
+                        }                    
+                      ?>
+                  </li>
                 </ul>
-
             </div>
         </div>
     </nav>
-    <div class="container  mySetting">
+    <div class="container  mySetting" style="margin-top: 150px">
       <h1 class="mb-4">Account Settings</h1>
       <div class="row">
         <div class="col-md-3">
@@ -182,14 +187,10 @@ require 'load-profile.php';
       </div>
     </div>
  </div>
-    <footer>
-        
-    </footer>
-
-  <!-- Bootstrap JavaScript -->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.1/dist/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-</body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  </body>
 </html>
 
